d471fec81e19675f23dd02f2f180ca48ee8b51d1
Gord_oe65 <Gord_oe65@student.itstep.org>
2023-04-12 18:36:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "To be, or not to be, that is the question," +
+                "\r\nWhether 'tis nobler in the mind to suffer" +
+                "\r\nThe slings and arrows of outrageous fortune," +
+                "\r\nOr to take arms against a sea of troubles," +
+                "\r\nAnd by opposing end them? To die: to sleep;" +
+                "\r\nNo more; and by a sleep to say we end" +
+                "\r\nThe heart-ache and the thousand natural shocks" +
+                "\r\nThat flesh is heir to, 'tis a consummation" +
+                "\r\nDevoutly to be wish'd. To die, to sleep\r\n";
+            Console.WriteLine(str);
+            int counter = 0;
+            str.Replace("die", "***");
+            foreach (char item in str)
+            {
+                if (item == '*')
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(str);
+            Console.WriteLine(counter);
+
+        }
+    }
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp14
+{
+    public delegate double CalcDelegate(double x, double y);
+    public class S
+    {
+        public double Plus(double x, double y)
+        {
+            return x + y;
+        }
+        public double Minus(double x, double y)
+        {
+            return x - y;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            S p = new S();
+            Write("Введите пример ");
+            string exp = ReadLine();
+            char s = ' ';
+            foreach (char item in exp)
+            {
+                if (item == '+' || item == '-')
+                {
+                    s = item; break;
+                }
+            }
+            string[] numbers = exp.Split(s);
+            CalcDelegate de = null;
+            switch (s)
+            {
+                case '+':
+                    {
+                        de = new CalcDelegate(p.Plus);
+                        break;
+                    }
+                case '-':
+                    {
+                        de = new CalcDelegate(p.Minus);
+                        break;
+                    }
+            }
+            WriteLine($"Result: {de(double.Parse(numbers[0]),double.Parse(numbers[1]))}");
+        }
+    }
+}
